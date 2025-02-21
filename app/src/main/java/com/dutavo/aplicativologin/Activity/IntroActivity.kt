0e368dab6c4e38380227898a17ed1e72c1c82cee
dutavo <luizdutavo12@gmail.com>
2025-02-20 21:06:45 -0300

    commit front

---
+++ app/src/main/java/com/dutavo/aplicativologin/Activity/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.dutavo.aplicativologin.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dutavo.aplicativologin.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.botaoInscrever.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+
+        }
+
+    }
+}
